a3b7ea1795cd8f43cdad5b10896cf769ee663172
umarfaruq-bilhack <princedelight4@gmail.com>
2026-06-07 20:22:44 +0100

    Fix npmrc and regenerate lockfile

--- fix.js
+++ fix.js
@@ -1,6 +1,4 @@
 ﻿const fs = require('fs');
-let c = fs.readFileSync('app/dashboard/DashboardClient.tsx', 'utf8');
-c = c.replace(/Creating bug art & tagging us/g, 'Create bug art & tag @TheChillBugs');
-c = c.replace(/creating bug art & tagging us/g, 'Create bug art & tag @TheChillBugs');
-fs.writeFileSync('app/dashboard/DashboardClient.tsx', c, 'utf8');
+fs.writeFileSync('.npmrc', 'legacy-peer-deps=true\n', 'utf8');
+fs.unlinkSync('package-lock.json');
 console.log('done');
